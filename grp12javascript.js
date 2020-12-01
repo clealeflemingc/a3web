b89a4ea988872ca5c0a97ee5dd1ab2699b250e8b
sarahgriffithsflemingc <sarah.griffiths@flemingcollege.ca>
2020-12-01 15:39:31 -0400

    Udpate Navbar for Responsive Design

---
+++ grp12javascript.js
@@ -0,0 +1,7 @@
+
+// Sarah's Script for Navbar, last modified 1-Dec-2020 //
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('mobilenavbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+navbarLinks.classList.toggle('active') } )
